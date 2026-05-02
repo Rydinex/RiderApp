d1dff24259a16593f84cd03f50340ccde4639690
Rydinex <Rydinextechnology@gmail.com>
2026-05-01 22:08:04 -0500

    Moved screens to src/screens and removed old rider-screens folder

---
+++ src/screens/AccountSettingsScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function AccountSettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Account Settings Screen</Text>
+    </View>
+  );
+}
